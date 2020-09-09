075082b9c12aad0a6abe417f24bde11e6821a73b
Emily Zheng <mzheng9@u.rochester.edu>
2020-09-09 15:08:14 -0400

    finally done with assign3 TAT

--- scripts.js
+++ scripts.js
@@ -1,47 +1,46 @@
 // don't execute any JS until after the DOM is loaded
 
 document.addEventListener("DOMContentLoaded", function() {
-	window.addEventListener('load', function () {
-  alert("It's loaded!")
-})
-	// create "handles" on the buttons from the HTML
-	var showNextIngredient = document.getElementById("showNextIngredient");
-	var showNextStep = document.getElementById("showNextStep");
 
+	clickToShow("showNextIngredient", "ingredientList");
+	clickToShow("showNextStep", "stepList");
+
+	
+});
+function clickToShow (button, list){
+	// create "handles" on the buttons from the HTML
+	var showNext = document.getElementById(button);
+	
+	
 	// create "handles" on the lists
-	var ingredientList = document.getElementById("ingredientList");
-	var stepList = document.getElementById("stepList");
+	var list = document.getElementById(list);
 
 	// get the lengths of the lists
-	var ingredientListLength = ingredientList.children.length;
-	var stepListLength = stepList.children.length;
+	var listLength = list.children.length;
+
 
 	// for each item in the lists, set their display value to "none" (hide them)
-	for (var i = 0; i < ingredientListLength; i++) {
-		ingredientList.children[i].style.display="none";
-	}
-	for (var i = 0; i < stepListLength; i++) {
-		stepList.children[i].style.display="none";
-	}
+	for (var i = 0; i < listLength; i++) {
+		list.children[i].style.display="none";
+	};
 
 	// setup a counter for the list
-	var ingredientListHead = 0;
+	var listHead = 0;
 	// wait for the user to click the button, then "revert" (show item) the next
+	
 	//. item in the list
-	showNextIngredient.addEventListener("click", function() {
-		if(ingredientListHead<ingredientListLength){
-			ingredientList.children[ingredientListHead].style.display="revert";
-			ingredientListHead++;
+	showNext.addEventListener("click", function() {
+		if(listHead<listLength){
+			list.children[listHead].style.display="revert";
+			listHead++;
 		}
 	});
+}
+function changeColor(obj){
+	if(obj.style.color == 'purple'){
+		obj.style.color = "orange";
+	}else{
+		obj.style.color = "purple";
+	}
+}
 
-	// setup a counter for the list
-	var stepListHead = 0;
-	// wait for the user to click the button, then "revert" (show item) the next
-	//. item in the list
-	showNextStep.addEventListener("click", function() {
-		if(stepListHead<stepListLength)
-		stepList.children[stepListHead].style.display="revert";
-		stepListHead++;
-	});
-});
