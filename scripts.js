f0496ad43da64ab8810fce2390a732a07430f92f
Emily Zheng <mzheng9@u.rochester.edu>
2020-09-16 08:38:23 -0400

    assignment 4, using Jquery for for the javascript, deleted the mouse hover functions

--- scripts.js
+++ scripts.js
@@ -1,46 +1,39 @@
 // don't execute any JS until after the DOM is loaded
 
-document.addEventListener("DOMContentLoaded", function() {
+$(document).ready(function() { 
 
-	clickToShow("showNextIngredient", "ingredientList");
-	clickToShow("showNextStep", "stepList");
-
+	$('#ingredientList>li').hide(); //hide the entire ingredient list
+	$('#stepList>li').hide();//hide the entire instruction list
 	
-});
-function clickToShow (button, list){
-	// create "handles" on the buttons from the HTML
-	var showNext = document.getElementById(button);
-	
-	
-	// create "handles" on the lists
-	var list = document.getElementById(list);
-
-	// get the lengths of the lists
-	var listLength = list.children.length;
-
-
-	// for each item in the lists, set their display value to "none" (hide them)
-	for (var i = 0; i < listLength; i++) {
-		list.children[i].style.display="none";
-	};
-
-	// setup a counter for the list
-	var listHead = 0;
-	// wait for the user to click the button, then "revert" (show item) the next
-	
-	//. item in the list
-	showNext.addEventListener("click", function() {
-		if(listHead<listLength){
-			list.children[listHead].style.display="revert";
-			listHead++;
-		}
+	$('#showNextIngredient').click(function(){ //when click
+		$('#ingredientList>li:hidden:first').show(); 
+		//show the first ingredient hidden in the list
 	});
-}
-function changeColor(obj){
-	if(obj.style.color == 'purple'){
-		obj.style.color = "orange";
-	}else{
-		obj.style.color = "purple";
+	$("#showNextStep").click(function(){ //when click 
+		$('#stepList>li:hidden:first').show(); 
+		//show the first step hidden
+	});
+
+});
+
+
+$('#AddStepButton').click(function (event) {//adding the click function
+	
+	if ($('#newStep').val()) {//check if user wants to add something
+		var toAdd = $('#newStep').val();//get user input
+		$('#stepList').append('<li>' + toAdd + '</li>');//add to the end
+		$('#newStep').val('');//set to blank
 	}
-}
+	return false;
+})
+
+$('#AddIngButton').click(function (event) {//adding the click function
+	
+	if ($('#newIngredient').val()) {//check if user wants to add 
+		var toAdd = $('#newIngredient').val();//get user input
+		$('#ingredientList').append('<li>' + toAdd + '</li>');//add to the end
+		$('#newIngredient').val('');//set to blank
+	}
+	return false;
+})
 
